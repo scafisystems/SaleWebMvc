b393c2e793d8e229e7b5b54b48fac54da3948b49
Scafisystems <62200377+scafisystems@users.noreply.github.com>
2020-07-16 18:55:19 +0200

    Other entities and second migration

--- SaleWebMvc/Data/SaleWebMvcContext.cs
+++ SaleWebMvc/Data/SaleWebMvcContext.cs
@@ -14,6 +14,8 @@ namespace SaleWebMvc.Data
         {
         }
 
-        public DbSet<SaleWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
